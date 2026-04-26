4f71fb94a81185394216b943565010e6f781f7a0
Otavio Vasques <otaviocv.deluqui@gmail.com>
2026-04-26 17:49:52 -0300

    Pouco progresso no item 10

--- src/g_matrizes/ex_10.zig
+++ src/g_matrizes/ex_10.zig
@@ -1,8 +1,17 @@
 // Exercício 10 de https://www.ime.usp.br/~macmulti/exercicios/matrizes/index.html
+// Uma matriz D_8x8 pode representar a posição atual de um jogo de damas, sendo que
+// `0` indica uma casa vazia, `1` indica uma casa ocupada por uma peça branca e `-1`
+// indica uma casa ocupada por uma peça preta. Supondo que as peças pretas estão se
+// movendo no sentido crescente das linhas da matriz $D$, determinar as
+// posições das peças pretas que:
+//
+// - a. podem tomar peças brancas;
+// - b. podem mover-se sem tomar peças;
+// - c. não podem se mover.
 
 const std = @import("std");
 const assert = std.debug.assert;
-const MAX: usize = 1000;
+const size: usize = 8;
 
 
 pub fn main() !void {
@@ -15,44 +24,25 @@ pub fn main() !void {
     var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
     const stdin = &stdin_reader.interface;
 
-    try stdout.print("n linhas do triangulo de pascal\nn > ", .{});
-    try stdout.flush();
-    const n_str = try stdin.takeDelimiterExclusive('\n');
-    const n = try std.fmt.parseInt(u32, n_str, 10);
-    stdin.toss(1);
-    assert(n <= MAX and n >= 1);
 
-    try stdout.print("Usando matriz:\n", .{});
-    var matriz: [MAX][MAX]i32 = undefined;
-    for (0..n) |i| {
-        for (0..(i+1)) |j| {
-            if (j == 0 or j == i) {
-                matriz[i][j] = 1;
-            } else {
-                matriz[i][j] = matriz[i-1][j] + matriz[i-1][j-1];
-            }
-            try stdout.print("{d} ", .{matriz[i][j]});
-        }
-        try stdout.print("\n", .{});
-    }
 
-    try stdout.print("Usando vetor:\n", .{});
-    var vetor: [MAX]i32 = undefined;
-    for (0..n) |i| {
-        const start_of_previous_row = @divFloor(i * i - i, 2);
-        const start_of_this_row = @divFloor(((i+1) * (i+1)) - (i+1), 2);
-        for (0..(i+1)) |j| {
-            const pos = start_of_this_row + j;
-            if (j == 0 or j == i) {
-                vetor[pos] = 1;
-            } else {
-                const left = start_of_previous_row + j - 1;
-                const right = left + 1;
-                vetor[pos] = vetor[left] + vetor[right];
-            }
-            try stdout.print("{d} ", .{vetor[pos]});
-        }
-        try stdout.print("\n", .{});
-    }
     try stdout.flush();
+}
+
+fn read_tabuleiro(container: *[MAX][MAX]i32, stdout: *std.Io.Writer, stdin: *std.Io.Reader) !void {
+    try stdout.print("valores > \n", .{});
+    try stdout.flush();
+
+    for (0..size) |i| {
+        for (0..(size-1)) |j| {
+            const num_str = try stdin.takeDelimiterExclusive(' ');
+            const num = try std.fmt.parseInt(i32, num_str, 10);
+            stdin.toss(1);
+            container.*[i][j] = num;
+        }
+        const num_str = try stdin.takeDelimiterExclusive('\n');
+        const num = try std.fmt.parseInt(i32, num_str, 10);
+        stdin.toss(1);
+        container.*[i][size-1] = num;
+    }
 }
